60f2693cd465ab1347ff013f51b7acc736423af7
Bhanu Singh <hey@bhanusingh.in>
2026-05-02 01:17:05 +0530

    fix: ts error

--- pages/api/cloudinary/[...media].ts
+++ pages/api/cloudinary/[...media].ts
@@ -1,8 +1,12 @@
-import { createMediaHandler, mediaHandlerConfig } from 'next-tinacms-cloudinary/dist/handlers';
+import { createMediaHandler } from 'next-tinacms-cloudinary/dist/handlers';
 
 import { isAuthorized } from '@tinacms/auth';
 
-export const config = mediaHandlerConfig;
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 export default createMediaHandler({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
