1ceb6ee83c32b1ae0feefc48e42586e5a249b8c6
Bhanu Singh <hey@bhanusingh.in>
2026-05-09 23:39:53 +0530

    feat: artwork blocks system with Header H2 block

---
+++ components/artwork-blocks/header-h2-block.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { tinaField } from 'tinacms/dist/react';
+import { type Components, TinaMarkdown } from 'tinacms/dist/rich-text';
+
+import type { ArtworkBlocksHeaderH2 } from '@/tina/__generated__/types';
+
+type Props = {
+  block: ArtworkBlocksHeaderH2;
+};
+
+const components: Components<{}> = {
+  p: (props) => <span className='block'>{props?.children}</span>,
+  break: () => <br />,
+  bold: (props) => <strong className='font-bold'>{props?.children}</strong>,
+  italic: (props) => <em className='italic font-sedan'>{props?.children}</em>,
+};
+
+export function HeaderH2Block({ block }: Props) {
+  if (!block.heading) return null;
+
+  return (
+    <h2 data-tina-field={tinaField(block, 'heading')} className='font-space-grotesk text-2xl tracking-[-0.04em] text-black'>
+      <TinaMarkdown content={block.heading} components={components} />
+    </h2>
+  );
+}
