52cda895611155bc31b2e86077b52276bd3f3111
Bhanu Singh <hey@bhanusingh.in>
2026-05-29 20:34:14 +0530

    fix: brand color on top loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
-import { fontSans, lato, nunito, sedan, spaceGrotesk } from '@/app/fonts';
-import { BaseLayout } from '@/components/base-layout';
-import { FaviconAnimator } from '@/components/favicon-animator';
-import { cn } from '@/lib/utils';
-import client from '@/tina/client';
-import type { Metadata } from 'next';
-import NextTopLoader from 'nextjs-toploader';
-import React from 'react';
+import { fontSans, lato, nunito, sedan, spaceGrotesk } from "@/app/fonts";
+import { BaseLayout } from "@/components/base-layout";
+import { FaviconAnimator } from "@/components/favicon-animator";
+import { cn } from "@/lib/utils";
+import client from "@/tina/client";
+import type { Metadata } from "next";
+import NextTopLoader from "nextjs-toploader";
+import React from "react";
 
-import '@/styles.css';
+import "@/styles.css";
 
 export const metadata: Metadata = {
   title: {
-    default: 'Prasun1111',
-    template: '%s - Prasun1111',
+    default: "Prasun1111",
+    template: "%s - Prasun1111",
   },
-  description: 'Official website of Prasun1111 featuring artworks, installations, films, design, and writings.',
+  description:
+    "Official website of Prasun1111 featuring artworks, installations, films, design, and writings.",
   icons: {
-    icon: [{ url: '/favicon/favicon_00.png', type: 'image/png' }],
+    icon: [{ url: "/favicon/favicon_00.png", type: "image/png" }],
   },
 };
 
@@ -26,22 +27,40 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const siteSettings = await client.queries.global(
-    { relativePath: 'site.json' },
+    { relativePath: "site.json" },
     {
       fetchOptions: {
         next: {
           revalidate: 60,
         },
       },
-    }
+    },
   );
 
   return (
-    <html lang='en' className={cn(fontSans.variable, nunito.variable, lato.variable, sedan.variable, spaceGrotesk.variable)}>
-      <body className='min-h-screen bg-background font-sans antialiased'>
-        <NextTopLoader color='#000000' showSpinner={false} height={2} shadow={false} />
+    <html
+      lang="en"
+      className={cn(
+        fontSans.variable,
+        nunito.variable,
+        lato.variable,
+        sedan.variable,
+        spaceGrotesk.variable,
+      )}
+    >
+      <body className="min-h-screen bg-background font-sans antialiased">
+        <NextTopLoader
+          color="#ff6400"
+          showSpinner={false}
+          height={2}
+          shadow={false}
+        />
         <FaviconAnimator />
-        <BaseLayout siteSettingsQuery={siteSettings.query} siteSettingsData={siteSettings.data} siteSettingsVariables={siteSettings.variables}>
+        <BaseLayout
+          siteSettingsQuery={siteSettings.query}
+          siteSettingsData={siteSettings.data}
+          siteSettingsVariables={siteSettings.variables}
+        >
           {children}
         </BaseLayout>
       </body>
